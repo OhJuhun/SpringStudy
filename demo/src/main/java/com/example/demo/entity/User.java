e3870f4ce0720bccb03b43d3713ae8e66062933d
OhJuhun <ojh0315@naver.com>
2020-06-14 19:33:05 +0900

    java spring boot

---
+++ demo/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,65 @@
+package com.example.demo.entity;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="uid")
+    private String uid;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="current")
+    private Long current;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(Long current) {
+        this.current = current;
+    }
+}
---
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,20 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+}
