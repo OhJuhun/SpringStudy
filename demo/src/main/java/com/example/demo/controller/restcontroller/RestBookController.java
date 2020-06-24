12871c340bbe1744c216f34fee73885cac0c5f3d
OhJuhun <ojh0315@naver.com>
2020-06-24 14:14:51 +0900

    rest api book CRUD fin

--- demo/src/main/java/com/example/demo/controller/restcontroller/RestBookController.java
+++ demo/src/main/java/com/example/demo/controller/restcontroller/RestBookController.java
@@ -2,13 +2,16 @@ package com.example.demo.controller.restcontroller;
 
 import com.example.demo.entity.Book;
 import com.example.demo.exception.NotFoundException;
+import com.example.demo.repository.BookRepository;
 import com.example.demo.service.BookService;
+import com.sun.media.sound.InvalidDataException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,4 +22,75 @@ public class RestBookController {
     @Autowired
     BookService bookService;
 
+    //CRUD만 만들기
+
+    /*
+     * TODO : CREATE NEW BOOK INFO
+     *  Created 201, Processed but not created 200, No Result to Return 204
+     *  Invalid Data 400
+     */
+    @PostMapping
+    public ResponseEntity createBook(@RequestBody Book book){
+        try {
+            bookService.insertBook(book);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+    /*
+     * TODO : GET ALL BOOKS
+     *  Return 200
+     *  Not Found 404
+     */
+    @GetMapping
+    public ResponseEntity getAllBooks(){
+        List<Book> books = bookService.getBooks();
+        if(books==null){
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity(books,HttpStatus.OK);
+    }
+
+    /*
+     * TODO : GET ONE BOOK BY ISBN
+     *  Return 200
+     *  Not Found 404
+     */
+    @GetMapping("/{isbn}") //return 200
+    public ResponseEntity getBookByIsbn(@PathVariable("isbn") String isbn){
+        Book book = bookService.getByIsbn(isbn);
+        if(book==null){
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity(book,HttpStatus.OK);
+    }
+
+    /*
+     * TODO : MODIFY BOOK INFO
+     *  Created 201, Updated 200 or 201, No Result to Return 204
+     *  Invalid Data 400, Conflict 409
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity modifyBook(@RequestBody Book book,
+                                     @PathVariable("id") Long id){
+        try{
+            bookService.modifyBook(id,book);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    /*
+     * TODO : DELETE BOOK BY ID
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteBook(@PathVariable("id") Long id){
+        return new ResponseEntity(HttpStatus.OK);
+    }
 }
--- demo/src/main/java/com/example/demo/repository/BookRepository.java
+++ demo/src/main/java/com/example/demo/repository/BookRepository.java
@@ -4,11 +4,11 @@ import com.example.demo.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book,Long>{
     Book findByIsbn(String isbn);
     Book findByName(String name);
-
 }
